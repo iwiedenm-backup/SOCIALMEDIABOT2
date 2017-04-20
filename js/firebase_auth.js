fc3aa39b3914ec33e8a7d2dea842a35d0d4a95ed
iwiedenm <iwiedenm@lion.lmu.edu>
2017-04-20 14:29:36 -0700

    app_sidebar_settings.html

--- js/firebase_auth.js
+++ js/firebase_auth.js
@@ -1,9 +1,22 @@
+// Initialize Firebase BANGERLIST
+// var config = {
+//   apiKey: "AIzaSyCf865M75QLYzGczbBeHbrX6_BEDV8pDic",
+//   authDomain: "bangerlist-d8359.firebaseapp.com",
+//   databaseURL: "https://bangerlist-d8359.firebaseio.com",
+//   storageBucket: "bangerlist-d8359.appspot.com",
+//   messagingSenderId: "990833137246"
+// };
+// firebase.initializeApp(config);
+
+//FIREBASE main email
+// Initialize Firebase
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCf865M75QLYzGczbBeHbrX6_BEDV8pDic",
-  authDomain: "bangerlist-d8359.firebaseapp.com",
-  databaseURL: "https://bangerlist-d8359.firebaseio.com",
-  storageBucket: "bangerlist-d8359.appspot.com",
-  messagingSenderId: "990833137246"
+  apiKey: "AIzaSyDAtNL5CeWTDpi9hHcnSUNzPUCOjBQacAg",
+  authDomain: "social-media-bot-a87f1.firebaseapp.com",
+  databaseURL: "https://social-media-bot-a87f1.firebaseio.com",
+  projectId: "social-media-bot-a87f1",
+  storageBucket: "social-media-bot-a87f1.appspot.com",
+  messagingSenderId: "523291917513"
 };
 firebase.initializeApp(config);
